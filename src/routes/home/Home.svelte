2c629bef92753ddfe3a3480ec2461135e9d740fd
Kris Fragata <kristinne.fragata@associatedenvironmentalsystems.com>
2024-06-23 20:00:37 -0400

    re(home page UI):Add a full works list button that leads to index

--- src/routes/home/Home.svelte
+++ src/routes/home/Home.svelte
@@ -18,10 +18,16 @@
 <SectionWrapper id="home">
 	<div class="max-h-[500px] flex flex-col gap-4 flex-1 overflow-hidden items-center justify-center">
 		<p
-			class="text-lg text-paragraphText font-semibold sm:text-xl md:text-2xl text-left max-w-[500px] mx-auto w-full py-10"
+			class="text-lg text-paragraphText font-semibold sm:text-xl md:text-2xl text-left max-w-[600px] mx-auto w-full"
 		>
 			I'm a <span class="text-highlight">software engineer</span> and
 			<span class="text-highlight">artist</span> based in Boston, Massachusetts and Manila, Philippines.
+		</p>
+
+		<p
+			class="text-lg text-paragraphText sm:text-2lg md:text-3lg text-left max-w-[600px] w-full"
+		> 
+		Full Works List
 		</p>
 	</div>
 </SectionWrapper>
